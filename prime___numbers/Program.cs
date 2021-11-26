fafa42aee265f29a54dda618d6723f9a676c7704
mohammed omran <mohammed omran@LAPTOP-PKK1I5EA>
2021-11-27 00:03:09 +0200

    Add project files.

---
+++ prime___numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime___numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 2; i < num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("it isn't a prime number");
+                    return;
+                }
+            }
+            Console.WriteLine("it is a prime number");
+        }
+    }
+}
